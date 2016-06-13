77e1ac80f0857d1fdaa6d7cc88fd0d1e558ff09e
RamiC <rami.chousein@gmail.com>
2016-06-13 11:52:46 +0300

    First commit. Pyramid alchemy template

---
+++ README.txt
@@ -0,0 +1,14 @@
+assignment_aa README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/pip install -e .
+
+- $VENV/bin/initialize_assignment_aa_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    ]
+
+setup(name='assignment_aa',
+      version='0.0',
+      description='assignment_aa',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='Rami Chousein',
+      author_email='rami.chousein@gmail.com',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = assignment_aa:main
+      [console_scripts]
+      initialize_assignment_aa_db = assignment_aa.scripts.initializedb:main
+      """,
+      )
